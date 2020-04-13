b220de9b95db657fbd4a116b852ff63f07c0e768
gerges12 <gergissaaid@gmail.com>
2020-04-13 14:14:30 +0200

    compelete backend feature one

---
+++ onlinebookstore/src/main/java/gerges/onlinebookstore/repositiry/BookCategoryRepository.java
@@ -0,0 +1,12 @@
+package gerges.onlinebookstore.repositiry;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import gerges.onlinebookstore.entity.BookCategory;
+
+@RepositoryRestResource(collectionResourceRel="bookCateogry", path="book-category")
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long>{
+
+}
